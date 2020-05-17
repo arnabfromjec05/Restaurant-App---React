833944518242aac1665584670df8598d5a9a6724
Arnab Jana <arnab.jana@walmartlabs.com>
2020-05-17 16:43:36 +0530

    single page application p2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -29,10 +29,19 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home 
-                dish={this.state.dishes.filter((dish)=> dish.featured===true)[0]}
-                promotion={this.state.promotions.filter((promotion)=> promotion.featured===true)[0]}
-                leader={this.state.leaders.filter((leader)=> leader.featured===true)[0]}
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured === true)[0]}
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured === true)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured === true)[0]}
+                />
+            );
+        }
+
+        const DishWithId = ({ match }) => {
+            return (
+                <SelectedDish 
+                dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
                 />
             );
         }
@@ -43,7 +52,8 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/contactus" component={() => <Contact />} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
